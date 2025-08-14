b425e37c607c86ee2ba0e99196350ce846c5ed34
Vatan009 <pawarvatan95@gmail.com>
2025-08-14 21:36:02 +0530

    added vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <App />
-  </StrictMode>,
+ 
 )
